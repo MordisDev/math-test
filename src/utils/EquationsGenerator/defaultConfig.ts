527bc3dc9901ca96b5bfbc1caf85bba3207ef69e
MordisDev <mordiskald@gmail.com>
2024-03-10 17:46:50 +0100

    simple config

--- src/utils/EquationsGenerator/defaultConfig.ts
+++ src/utils/EquationsGenerator/defaultConfig.ts
@@ -1,9 +1,9 @@
 import { GeneratorConfigProps } from "./schema"
 
 const defaultConfig: GeneratorConfigProps = {
-    equationsAmount: 12,
-    maxParameterValue: 24,
-    parametersAmount: 3,
+    equationsAmount: 8,
+    maxParameterValue: 10,
+    parametersAmount: 2,
     isParametersAmountStrict: false,
     equationsType: [{type: '+', ratio: 100},{type: '-', ratio: 0}],
     isPossibleNegativeResult: false,
